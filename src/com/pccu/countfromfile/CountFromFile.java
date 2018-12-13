694ed620c02e38bd8b8722940529363bc7c51036
SongYoSCY <A6248660@g.pccu.edu.tw>
2018-12-13 13:59:15 +0800

    second commit

--- src/com/pccu/countfromfile/CountFromFile.java
+++ src/com/pccu/countfromfile/CountFromFile.java
@@ -8,9 +8,9 @@ public class CountFromFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			Scanner scanner = null;
+				Scanner scanner = null;
 			int mSum = 0;
-			int mAmount = 0;
+				int mAmount = 0;
 			String mCustomer = "";
 		try {
 			scanner = new Scanner(new File("src/consume.txt"));
